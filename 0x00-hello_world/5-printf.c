30b7ee7591dd7e398e2d0161f2bad1bd3c884468
gabrieludeme <gabrieludeme@gmail.com>
2022-09-09 07:02:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string
+ * Desrciption: use printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a place of art,\n");
+	return (0);
+}
