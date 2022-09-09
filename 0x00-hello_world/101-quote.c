e06e35a4b4e307e4e493928b1beb909f3b45f8ac
gabrieludeme <gabrieludeme@gmail.com>
2022-09-09 08:45:15 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unlistd.h>
+
+/**
+ * main - print quote
+ * Description: prints Dor's quote
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
